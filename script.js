55bd8fc392990082f0b778400532b576e7417017
masooma-jw <masoomajw@gmail.com>
2023-07-19 15:17:10 +0530

    nothing added

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 var questions = [
   {
     question: "When was javascript invented?",
@@ -67,17 +69,25 @@ const submit = document.getElementById("submit");
 
 var currentQuestion = 0; // Tracks the current question
 var score = 0; // Tracks the score
+var userAnswers =[];
 
 function displayQuestion() {
   var questionElement = document.getElementById("question");
   var choiceElements = document.getElementsByTagName("label"); 
 
   questionElement.textContent = questions[currentQuestion].question;
-
+ 
 
   for (var i = 0; i < choiceElements.length; i++) {
     choiceElements[i].textContent = questions[currentQuestion].choices[i];
   }
+
+// Check if the user has answered this question previously
+var previousAnswer = userAnswers[currentQuestion];
+if (previousAnswer !== undefined) {
+  var choices = document.getElementsByName("choice");
+  choices[previousAnswer].checked = true;
+}
 
   if (currentQuestion == 0) {
     previous.style.display = "none";
@@ -101,17 +111,20 @@ submit.addEventListener("click", () => {
       
       break;
     }
+  
   }
 
   if (selectedChoice == -1) {
     alert("Please select an option.");
     return;
   }
-
+  userAnswers[currentQuestion]=selectedChoice;
+ 
  
 
   if (selectedChoice == questions[currentQuestion].answer) {
     score++;
+    
     
   }
 
@@ -133,13 +146,14 @@ submit.addEventListener("click", () => {
     choices[i].checked = false
     
   }
+ 
 });
 
 function showPreviousQuestion() {
   currentQuestion--;
+  
   displayQuestion();
 }
-
 
 
 function shuffleQuestions() {
@@ -154,3 +168,180 @@ previous.addEventListener("click", showPreviousQuestion)
 
 shuffleQuestions();
 displayQuestion();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// //Retrieve stored user data from sessionStorage
+// let currentUserData = JSON.parse(sessionStorage.getItem('userData')) || {};
+
+// // Function to save user data in sessionStorage
+// function saveUserData() {
+//   sessionStorage.setItem('userData', JSON.stringify(currentUserData));
+// }
+
+// // Function to display the login page
+// function showLoginPage() {
+//   // Restoring user data from sessionStorage
+//   currentUserData = JSON.parse(sessionStorage.getItem('userData')) || {};
+
+//   document.getElementById('loginPage').style.display = 'block';
+//   document.getElementById('quizPage').style.display = 'none';
+//   document.getElementById('scorePage').style.display = 'none';
+// }
+
+// // Function to display the quiz page
+// function showQuizPage() {
+//   document.getElementById('loginPage').style.display = 'none';
+//   document.getElementById('quizPage').style.display = 'block';
+//   document.getElementById('scorePage').style.display = 'none';
+// }
+
+// // Function to display the score page
+// function showScorePage() {
+//   document.getElementById('loginPage').style.display = 'none';
+//   document.getElementById('quizPage').style.display = 'none';
+//   document.getElementById('scorePage').style.display = 'block';
+
+//   // Display the final score
+//   document.getElementById('finalScore').innerText = `Your Score: ${currentUserData.score}`;
+
+//   // Clear sessionStorage since the game is completed
+//   sessionStorage.removeItem('userData');
+// }
+
+// // Function to handle the form submission and start the quiz
+// function startQuiz(event) {
+//   event.preventDefault();
+
+//   // Get the entered username and email
+//   const username = document.getElementById('username').value;
+//   const email = document.getElementById('email').value;
+
+//   // Check if the user data already exists in sessionStorage
+//   if (currentUserData.email === email) {
+//     // User data exists, proceed to the quiz page
+//     showQuizPage();
+//     displayQuestion();
+//   } else {
+//     // User data doesn't exist, create a new entry
+//     currentUserData = { username, email, score: 0 };
+//     saveUserData();
+
+//     // Proceed to the quiz page
+//     showQuizPage();
+//     displayQuestion();
+//   }
+// }
+
+// // Restoring user data from sessionStorage when the page loads
+// window.addEventListener('load', showLoginPage);
+
+// // ... Rest of the code remains the same ...
+
+
+
+
+
+// Function to save the current question index and user answers in sessionStorage
+function saveProgress() {
+  sessionStorage.setItem('currentQuestionIndex', currentQuestionIndex);
+  sessionStorage.setItem('userAnswers', JSON.stringify(userAnswers));
+}
+
+// Function to retrieve the saved progress from sessionStorage
+function retrieveProgress() {
+  const savedQuestionIndex = sessionStorage.getItem('currentQuestionIndex');
+  const savedUserAnswers = sessionStorage.getItem('userAnswers');
+
+  if (savedQuestionIndex && savedUserAnswers) {
+    currentQuestionIndex = parseInt(savedQuestionIndex);
+    userAnswers = JSON.parse(savedUserAnswers);
+  }
+}
+
+// Function to display the login page or resume the quiz
+function showLoginPageOrResumeQuiz() {
+  if (currentQuestionIndex === 0 && userAnswers.length === 0) {
+    showLoginPage();
+  } else {
+    showQuizPage();
+    displayQuestion();
+  }
+}
+
+// Function to go to the next question
+function nextQuestion() {
+  currentQuestionIndex++;
+
+  if (currentQuestionIndex === questions.length) {
+    calculateScore();
+    showScorePage();
+  } else {
+    displayQuestion();
+  }
+
+  saveProgress();
+}
+
+// Function to go to the previous question
+function previousQuestion() {
+  currentQuestionIndex--;
+  displayQuestion();
+  saveProgress();
+}
+
+// Add event listener to the window object to handle page refresh
+window.addEventListener('beforeunload', saveProgress);
+
+// Initialization
+retrieveProgress();
+showLoginPageOrResumeQuiz();
